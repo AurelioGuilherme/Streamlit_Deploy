87af19bef62be4185a1eafc899128de10cd7a032
AurelioGuilherme <109996864+AurelioGuilherme@users.noreply.github.com>
2024-04-09 14:12:28 -0300

    add notebook

--- pages/3_ 🧠 Modelos_de_Deep_Learning.py
+++ pages/3_ 🧠 Modelos_de_Deep_Learning.py
@@ -1,13 +1,29 @@
 import streamlit as st
 import torch
+from streamlit_option_menu import option_menu
+from nbconvert import HTMLExporter
+import nbformat
+import codecs
 
-PAGE_TITLE = 'Modelos de Deep Learning  🧠 '
+PAGE_TITLE = 'Modelos de Deep Learning 🧠'
 PAGE_ICON = "🧠"
+MENU_LIST = ['Sobre',
+             "1 - Visão computacional - ResNet"]
+ICON_LIST = ["🧠","👀"]
+
 st.set_page_config(page_title=PAGE_TITLE,page_icon=PAGE_ICON)
 
-st.title('Modelos de Deep Learning')
 
-st.write('''
+with st.sidebar:
+    selected = option_menu("",
+                           MENU_LIST,icons=ICON_LIST,
+                           default_index=0)
+    
+def main():
+    if selected == 'Sobre':
+        st.title('Modelos de Deep Learning')
+
+        st.write('''
             Neste espaço, estou compartilhando diversos projetos nos quais utilizei o 
             PyTorch para explorar uma variedade de problemas de Deep Learning. Desde 
             tarefas mais simples, como classificação e regressão, até abordagens mais 
@@ -18,6 +34,24 @@ st.write('''
             mas também meu compromisso em encontrar soluções eficazes e inovadoras para 
             problemas do mundo real. Espero que esses exemplos demonstrem minha habilidade 
             em lidar com dados, minha criatividade na abordagem de desafios e minha dedicação 
-            em entregar resultados de alta qualidade.
-            
-        ''')
+            em entregar resultados de alta qualidade.            
+                ''')
+        
+    elif selected == '1 - Visão computacional - ResNet':
+        st.title('Notebook Jupyter')
+
+    # Carregando o notebook
+    with codecs.open('Notebook/teste_notebook.ipynb', 'r', 'utf-8') as notebook_file:
+        notebook_content = notebook_file.read()
+        notebook = nbformat.reads(notebook_content, as_version=4)
+
+    # Convertendo o notebook para HTML
+    html_exporter = HTMLExporter()
+    html_body, _ = html_exporter.from_notebook_node(notebook)
+
+    # Exibindo o conteúdo do notebook como HTML
+    st.components.v1.html(html_body, width=800, height=600, scrolling=True)
+
+
+if __name__ == "__main__":
+    main()
