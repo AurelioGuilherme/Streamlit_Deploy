987b9cf12dd806adc8d2f7d277940817d32b46cd
AurelioGuilherme <109996864+AurelioGuilherme@users.noreply.github.com>
2024-04-13 19:23:47 -0300

    Deploy model

--- pages/3_ 🧠 Modelos_de_Deep_Learning.py
+++ pages/3_ 🧠 Modelos_de_Deep_Learning.py
@@ -15,23 +15,120 @@ ICON_LIST = ["🧠","👀"]
 st.set_page_config(page_title=PAGE_TITLE,page_icon=PAGE_ICON, layout="wide")
 
 # --- LOAD MODEL ---
+# Sistema e manipulação de arquivos
+import os
+import warnings
+warnings.filterwarnings("ignore")
+
+# Manipulação e processamento de dados
+import numpy as np
+from pl_bolts.transforms.dataset_normalizations import cifar10_normalization
+
+# Modelagem
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from pytorch_lightning import LightningModule
 
 
+from torch.optim.lr_scheduler import OneCycleLR
 
 
+# Avaliação do modelo
+from torchmetrics.classification import Accuracy
+
+# Visualização de dados
+import matplotlib.pyplot as plt
+from torchvision import datasets
+from PIL import Image
 
 
+# Módulo para carregar um modelo pré-treinado de arquitetura ResNet sem os pesos (queremos somente a arquitetura)
+def carrega_modelo_pretreinado():
+    modelo = torchvision.models.resnet18(weights = None, num_classes = 10)
+    modelo.conv1 = nn.Conv2d(3, 64, kernel_size = (3, 3), stride = (1, 1), padding = (1, 1), bias = False)
+    modelo.maxpool = nn.Identity()
+    return modelo
 
-# --- LOAD DATA ---
+# Classe com Arquitetura do Modelo
+class ModeloResnet(LightningModule):
+    
+    # Método construtor
+    def __init__(self, lr = 0.05):
+        super().__init__()
+        self.save_hyperparameters()
+        self.model = carrega_modelo_pretreinado()
 
-with open('./Data/cifar10/dados_test', mode = 'rb') as file:
-    data = pickle.load(file, encoding = 'bytes')
+    # Método Forward
+    def forward(self, x):
+        out = self.model(x)
+        return F.log_softmax(out, dim = 1)
 
-X = data[b'data']
-y = np.array(data[b'labels'])
-raw_images = X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("uint8")
+    # Método de um passo de treinamento
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        self.log("train_loss", loss)
+        return loss
 
+    # Método de avaliação
+    def evaluate(self, batch, stage = None):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim = 1)
+        accuracy = Accuracy(task = "multiclass", num_classes = 10).to(device)
+        acc = accuracy(preds, y)
 
+        if stage:
+            self.log(f"{stage}_loss", loss, prog_bar = True)
+            self.log(f"{stage}_acc", acc, prog_bar = True)
+
+    # Método de um passo de validação
+    def validation_step(self, batch, batch_idx):
+        self.evaluate(batch, "val")
+
+    # Método de um passo de teste
+    def test_step(self, batch, batch_idx):
+        self.evaluate(batch, "test")
+
+    # Método de configuração do otimizador
+    def configure_optimizers(self):
+        
+        # Otimização SGD
+        optimizer = torch.optim.SGD(self.parameters(), 
+                                    lr = self.hparams.lr, 
+                                    momentum = 0.9, 
+                                    weight_decay = 5e-4)
+        
+        # Passos por época
+        steps_per_epoch = 45000 // BATCH_SIZE
+        
+        # Scheduler
+        scheduler_dict = {
+            "scheduler": OneCycleLR(optimizer,
+                                    0.1,
+                                    epochs = self.trainer.max_epochs,
+                                    steps_per_epoch = steps_per_epoch),
+            "interval": "step",
+        }
+        
+        return {"optimizer": optimizer, "lr_scheduler": scheduler_dict}
+
+test_data_pipeline = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.ToTensor(),
+        cifar10_normalization()
+    ]
+)
+
+testset  = datasets.CIFAR10(root='./Data/cifar10/teste',train = False,download=False, transform=test_data_pipeline)
+testloader = torch.utils.data.DataLoader(testset, batch_size=10, shuffle=True)
+model = ModeloResnet()
+model.load_state_dict(torch.load('./models/image_recognition/saved_models/modelo_dl.pth'))
+model.eval()
 
 
 
@@ -109,29 +206,38 @@ def main():
                    7: 'Cavalo',
                    8: 'Navio',
                    9: 'Caminhão'}
-
-
-
-
-
         
-        if st.button('Selecionar 10 Imagens Aleatórias'):
-           # Selecionar 10 índices aleatórios
-           random_indices = np.random.choice(len(raw_images), size=10, replace=False)
-           
-           # Exibir as imagens e as classes previstas
-           columns = st.columns(10)  # Dividir a largura disponível em 10 colunas
+        resultado = ['❌','✅']
 
-           # Exibir as imagens selecionadas em colunas separadas       
-           for i, idx in enumerate(random_indices):
-                with columns[i]:  # Exibir cada imagem em uma coluna separada
-                    st.image(raw_images[idx], caption=f'Imagem {idx+1}: {classes[y[idx]]}', width=100, use_column_width=False)
+
+        dataiter = iter(testloader)
+        images, labels = next(dataiter)
+        tamanho = int(len(images))
+        columns = st.columns(10)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1) 
+
+        def imshow(img):
+            # Unnormalize the image
+            img = (img / 2) + 0.5
+            # Clip the image data to ensure it's within [0.0, 1.0]
+            img = np.clip(img, 0.0, 1.0)
+            # Convert from tensor to numpy array
+            img = img.numpy()
+            return np.transpose(img, (1, 2, 0))
+        
+         
+        if st.button('Selecionar 10 Imagens Aleatórias'):
+                      
+            for i in range(tamanho):
+                foto = images[i] / 2 + 0.5
+                
+                with columns[i]:
+                    st.image(imshow(foto), width=100)
+                    st.write('**CLASSE REAL:**\n',classes[labels[i].item()], '\n','\n**PREDIÇÃO:**\n',classes[predicted[i].item()],  f'{resultado[predicted[i].item() == labels[i].item()]}')                    
                     
 
-
         st.write('---')
-
-        
 
 
         # Carregando notebook no streamlit com expander
